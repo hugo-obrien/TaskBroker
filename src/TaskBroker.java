8d8b9023657281af378b97f6cd7899899dc017fc
Sergey Gubenko <gubenko@vologda.softlab.ru>
2018-09-14 14:45:01 +0300

    Поддержка Map

--- src/TaskBroker.java
+++ src/TaskBroker.java
@@ -2,14 +2,14 @@ import javafx.util.Pair;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.*;
 
 public class TaskBroker {
 
-    private static final TaskBroker BROKER = new TaskBroker();
     private static final int THREAD_POOL_SIZE = 5;
+
+    private static final TaskBroker BROKER = new TaskBroker();
     private static final ScheduledExecutorService THREAD_POOL = Executors.newScheduledThreadPool(THREAD_POOL_SIZE);
 
     public synchronized Future addTask(Pair<LocalDateTime, Callable> pair) {
@@ -20,12 +20,21 @@ public class TaskBroker {
         return addTask(task, getDelay(dateTime));
     }
 
-    public synchronized List<Future> addBacklog(List<Pair<LocalDateTime, Callable>> backlog) {
+    public synchronized List<Future> addAll(Collection<Pair<LocalDateTime, Callable>> backlog) {
         List<Future> futures = new ArrayList<>();
         for (Pair<LocalDateTime, Callable> pair : backlog) {
             futures.add(addTask(pair));
         }
         return futures;
+    }
+
+    public synchronized Map<LocalDateTime, Future> addAll(Map<LocalDateTime, Callable> map) {
+        Map<LocalDateTime, Future> futureMap = new HashMap<>();
+        map.forEach((key, value) -> {
+            Future future = addTask(key, value);
+            futureMap.put(key, future);
+        });
+        return futureMap;
     }
 
     public static TaskBroker getInstance() {
